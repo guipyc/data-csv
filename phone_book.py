8962929dc9d1970be2e2960a1fa077206eb32845
Guilherme Barreto <guilhermeofficial.35@gmail.com>
2022-10-05 12:45:39 -0300

    Progress

--- phone_book.py
+++ phone_book.py
@@ -1,1 +1,8 @@
+import csv
 
+with open('data/phone_book.csv', mode='r') as csv_file:
+    reader = csv.DictReader(csv_file, skipinitialspace=True)
+    for row in reader:
+        print(row["first_name"],row["last_name"],row["phone_number"])
+
+print(type(row))
